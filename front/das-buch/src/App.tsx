b8fe26c939a7e1ab2557d34bb492be6d97ebcbf5
Matheqs <matheqs@outlook.com>
2025-04-25 01:03:31 -0300

    First front end commit - Vite + React + Typescript

---
+++ front/das-buch/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import './index.css'; 
+function App() { 
+return ( 
+<div className="min-h-screen flex items-center justify-center bg-gray-100 text-2xl"> 
+<h1 className="text-blue-600 font-bold">Projeto React + Vite funcionando! 🚀</h1>
+</div> 
+); 
+} 
+
+export default App
