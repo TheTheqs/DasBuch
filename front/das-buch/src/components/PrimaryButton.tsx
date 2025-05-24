ee00dc95f46a3fc4af8522883b1127e477623d4b
Matheqs <matheqs@outlook.com>
2025-05-24 01:59:31 -0300

    Removing old tests

--- front/das-buch/src/components/PrimaryButton.tsx
+++ front/das-buch/src/components/PrimaryButton.tsx
@@ -7,7 +7,17 @@ interface PrimaryButtonProps {
 
 function PrimaryButton({ to, label }: PrimaryButtonProps) {
   return (
-    <Link to={to} className="btn btn-darksoft btn-lg px-4 mb-4">
+    <Link
+      to={to}
+      className="btn btn-darksoft btn-lg px-4 mb-4"
+      style={{
+        minWidth: "200px",
+        maxWidth: "320px",
+        whiteSpace: "normal",
+        wordBreak: "break-word",
+        textAlign: "center",
+      }}
+    >
       {label}
     </Link>
   );
